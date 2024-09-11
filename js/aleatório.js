c1db1b74af85ed8482b27b7939bf46140a254025
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-09-11 07:58:36 -0300

    Update aleatório.js

--- js/aleatório.js
+++ js/aleatório.js
@@ -1,4 +1,8 @@
+const nomes = ["amanda", "Luiza", "Ricardo", "Juliana", "Thiago", "Lucas"];
+
 export function aleatorio (list){
     const posicao = Math.floor(Math.random()* lista.lenght);
     return lista[posicao];
 }
+
+export const nome = aleatorio (nomes)
