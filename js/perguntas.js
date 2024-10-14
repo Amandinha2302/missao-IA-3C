265d83575589a178cddfc6f21c578c21819d94d1
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-10-14 19:05:59 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -6,18 +6,17 @@ export const perguntas = [
                 texto: "Baleia-azul",
                 afirmacao: [
                     " A baleia-azul pode atingir até 30 metros de comprimento e pesar até 200 toneladas, tornando-se o maior animal conhecido na Terra.",
-                   
-                    ],
+                ],
+                proxima: 1, 
             },
             {
                 texto: "Elefante-africano:",
                 afirmacao: [
                     " Embora o elefante-africano seja o maior mamífero terrestre, a baleia-azul é o maior animal em geral, tanto terrestre quanto aquático.",
-                 
-                    ],
-            },           
-            
-        ]
+                ],
+                proxima: 1, 
+            },             
+        ],
     },
     {
         enunciado: "Agora você quer saber qual dos qual dos seguintes mamíferos é conhecido por suas habilidades extraordinárias de camuflagem e adaptação ao ambiente?",
@@ -26,17 +25,15 @@ export const perguntas = [
                 texto:"Tigre",
                 afirmacao: [
                     "Lince",
-                    "afirmacao 2"
-                    ]
+                ],
             },
             {
                 texto: "Lince",
                 afirmacao: [
                     "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
+                ],
+            },
+        ],
     },
     {
         enunciado: "Pergunta 3",
@@ -45,17 +42,14 @@ export const perguntas = [
                 texto:"Alternativa 1 da pergunta 3",
                 afirmacao: [
                     "afirmacao 1",
-                    "afirmacao 2"
-                    ]
+                ],
             },
             {
                 texto:"Alternativa 2 da pergunta 3",
                 afirmacao: [
                     "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
+                ],
+            },  
+        ],
     },
 ];
